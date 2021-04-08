8290d3149ccc8c89157d0c878d1609e81132dcbd
raselkobir <raselkobir57@gmail.com>
2021-04-08 23:42:31 +0600

    mvc-setup

--- Web.BookStore/Startup.cs
+++ Web.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace Web.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,17 +25,13 @@ namespace Web.BookStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseRouting();
             }
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
